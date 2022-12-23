b6f1f10244cc1f4688c47382da8df88e4bdcf5bf
Aji <atamasite@gmail.com>
2022-12-23 10:23:17 +0700

    add models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserRole(models.Model):
+    ROLE_CHOICES = (
+        (1, 'Student'),
+        (2, 'Librarian')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    total_copies = models.PositiveIntegerField()
+    available_copies = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('title', 'author',)
+
+    def __str__(self):
+        return self.title
+
+
+class Borrow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField()
+    due_at = models.DateTimeField()
+    returned_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('user', 'book',)
+
+    def __str__(self):
+        return f"{self.user} - {self.book}"
